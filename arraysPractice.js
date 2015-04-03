71750d09984b650eb16d0037fe2fe6bc77240592
Edgar <ew1773@gmail.com>
2015-04-03 14:35:06 -0600

    reversedLooper Complete

--- arraysPractice.js
+++ arraysPractice.js
@@ -5,8 +5,10 @@ var arr = [10,20,30];
 //Create a function named 'first' that is given 'arr' as the argument and  returns the first item the given array.
 
   //Code Here
-
-
+var first = function(){
+  return arr[0];
+}
+first()
 //Next problem
 
 
@@ -14,9 +16,11 @@ var arr = [10,20,30];
 var arr = [40,50,60];
 //Create a function named 'last' that is given 'arr' as the argument and returns the last item the given array.
 
-
-  //Code Here
-
+var arr = [40,50,60];
+ var last = function(){
+  return arr.length - 1;
+ }
+ last();
 
 //Next Problem
 
@@ -24,8 +28,12 @@ var arr = [40,50,60];
 var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 //Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
 
-  //Code Here
-
+var looper = function(){
+  for (var i = 0; i < family.length; i++) {
+    alert(family[i]);
+  };
+};
+looper();
 
 //Next problem
 
@@ -33,10 +41,12 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 var letters = ['A', 'B', 'C', 'D', 'E'];
 //Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards alerting every item in the array starting at the end.
-
-  //Code Here
-
-
+var reversedLooper = function(){
+  for (var i = letters.length -1; i >= 0; i--) {
+    alert(letters[i]);
+  };
+};
+reversedLooper();
 //Next Problem
 
 
